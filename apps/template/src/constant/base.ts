c6fab292123382cd25691fb78cc622b06fd7fcc3
wyw <740484522@qq.com>
2023-09-01 13:58:31 +0800

    ci(template): 安装vite-plugin-vue-devtools工具

--- apps/template/src/constant/base.ts
+++ apps/template/src/constant/base.ts
@@ -5,11 +5,3 @@ export enum PERMISSION_TYPE {
     'PC' = 1,
     'APP' = 2,
 }
-
-/**
- * 检查项正常异常 1.正常，0.异常
- */
-export enum InspectionItemEnum {
-    正常 = 1,
-    异常 = 0,
-}
--- apps/template/vite.config.ts
+++ apps/template/vite.config.ts
@@ -8,6 +8,7 @@ import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 import Inspect from 'vite-plugin-inspect';
 import legacyPlugin from '@vitejs/plugin-legacy';
+import VueDevTools from 'vite-plugin-vue-devtools';
 
 const pathSrc = path.resolve(__dirname, 'src');
 
@@ -18,26 +19,25 @@ export default defineConfig({
         },
     },
     plugins: [
+        VueDevTools(),
         Vue(),
         AutoImport({
-            // Auto import functions from Vue, e.g. ref, reactive, toRef...
             // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
             imports: ['vue'],
 
-            // Auto import functions from Element Plus, e.g. ElMessage, ElMessageBox... (with style)
             // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
-            // resolvers: [ElementPlusResolver()],
+            resolvers: [ElementPlusResolver()],
+
             dts: path.resolve(pathSrc, 'types/auto-imports.d.ts'),
         }),
 
         Components({
             resolvers: [
-                // Auto register icon components
                 // 自动注册图标组件
                 IconsResolver({
                     enabledCollections: ['ep'],
                 }),
-                // Auto register Element Plus components
+
                 // 自动导入 Element Plus 组件
                 ElementPlusResolver(),
             ],
