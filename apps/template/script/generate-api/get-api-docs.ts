bc5a9b30e87f52d1483fc2f245590ff155f5bd83
wyw <740484522@qq.com>
2023-09-01 13:46:00 +0800

    ci(generate-template): rename

--- apps/template/script/generate-api/get-api-docs.ts
+++ apps/template/script/generate-api/get-api-docs.ts
@@ -1,6 +1,6 @@
 import { writeFileSync } from 'fs';
 import axios, { AxiosHeaders } from 'axios';
-type unknowType = any;
+
 interface tagType {
     name: string;
     description: string;
@@ -29,28 +29,26 @@ interface swaggerResType {
 
 async function writeAPI() {
     try {
-        // 将需要用到的后端swagger接口地址放进来
-        const apiList = [
-            'http://192.168.3.201:8002/v3/api-docs?group=%E7%B3%BB%E7%BB%9F%E6%9C%8D%E5%8A%A1',
-        ];
+        // 将需要用到的后端 swagger 接口地址放进来
+        const apiList = ['http://192.168.3.201:8002/v3/api-docs?group=%E7%B3%BB%E7%BB%9F%E6%9C%8D%E5%8A%A1'];
 
-        // 收集所有后端swagger返回的数据
-        const res: unknowType = await Promise.all(
-            apiList.map(async (items) => {
-                const respon = await axios.get(items as string);
-                if (!respon) {
+        // 收集所有后端 swagger 返回的数据
+        const res: any = await Promise.all(
+            apiList.map(async items => {
+                const response = await axios.get(items as string);
+                if (!response) {
                     return [];
                 }
-                return respon;
-            })
+                return response;
+            }),
         );
 
         // 需要合并的路径对象
         let pathObj = {};
-        // 需要合并的tags数组
+        // 需要合并的 tags 数组
         let tagsList: tagType[] = [];
 
-        // 合并路径对象以及tags数组
+        // 合并路径对象以及 tags 数组
         res.forEach((item: swaggerResType) => {
             pathObj = {
                 ...pathObj,
@@ -59,7 +57,7 @@ async function writeAPI() {
             tagsList = [...tagsList, ...item.data.tags];
         });
 
-        // 取第一个人的作为模板，将合并过的paths和tags替换了
+        // 取第一个人的作为模板，将合并过的 paths 和 tags 替换了
         const conversionData = res.shift()?.data;
         conversionData.paths = pathObj;
         conversionData.tags = arrayDeduplicate(tagsList, 'name');
@@ -71,7 +69,7 @@ async function writeAPI() {
         for (const key in writeData.paths) {
             const funName = key
                 .split('/')
-                .filter((item) => item && item !== 'api' && item !== 'v1')
+                .filter(item => item && item !== 'api' && item !== 'v1')
                 .map((item, index) => {
                     if (index === 0) {
                         return item;
@@ -98,10 +96,10 @@ writeAPI();
  * @param arr 需要去重的对象数组
  * @returns hashList 返回去重后的列表
  */
-export function arrayDeduplicate(arr: unknowType[], name: string) {
-    const hash: unknowType = {};
+export function arrayDeduplicate(arr: any[], name: string) {
+    const hash: any = {};
     let hashList = JSON.parse(JSON.stringify(arr));
-    hashList = hashList.reduce((total: unknowType, currentValue: unknowType) => {
+    hashList = hashList.reduce((total: any, currentValue: any) => {
         if (!hash[currentValue[name]]) {
             hash[currentValue[name]] = true;
             total.push(currentValue);
